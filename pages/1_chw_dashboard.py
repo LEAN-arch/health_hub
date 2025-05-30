489dcae836bed1a9b3dae1015d646bdc7776c5a9
LEAN-arch <jbautistamsc@gmail.com>
2025-05-29 22:29:41 -0700

    Update 1_chw_dashboard.py

--- pages/1_chw_dashboard.py
+++ pages/1_chw_dashboard.py
@@ -54,7 +54,7 @@ else:
     st.markdown("---") 
 
     # --- Sidebar Filters ---
-    st.sidebar.image("assets//DNA-DxBrand.png", width=300)
+    st.sidebar.image("assets//DNA-DxBrand.png", width=200)
     st.sidebar.header("🗓️ CHW Filters") 
     
     min_date_available_chw = None
